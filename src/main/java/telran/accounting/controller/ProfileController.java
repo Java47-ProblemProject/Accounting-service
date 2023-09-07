217616920f8b13fad907f488895abc4f09fb8f5f
stas <rsl14591@gmail.com>
2023-09-07 14:26:21 +0300

    07.09.2023 Token verification repaired

--- src/main/java/telran/accounting/controller/ProfileController.java
+++ src/main/java/telran/accounting/controller/ProfileController.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-//@CrossOrigin(origins = "http://127.0.0.1:5173", allowedHeaders = "*")
+@CrossOrigin(origins = "http://127.0.0.1:5173", allowedHeaders = "*")
 public class ProfileController {
     final ProfileService profileService;
 
